b400177aa6f81a7098d404fa4d3a71b9969f127f
Olyvia <14greeno@gmail.com>
2026-05-10 21:06:36 -0500

    Allow feat and heritage homebrew types

---
+++ supabase/migrations/20260510000000_homebrew_feats_heritages.sql
@@ -0,0 +1,8 @@
+-- Allow the generic homebrew_entries table to store new web-created
+-- Pathfinder 2e content categories.
+ALTER TABLE public.homebrew_entries
+  DROP CONSTRAINT IF EXISTS homebrew_entries_type_check;
+
+ALTER TABLE public.homebrew_entries
+  ADD CONSTRAINT homebrew_entries_type_check
+  CHECK (type IN ('monster', 'spell', 'item', 'feat', 'heritage'));
